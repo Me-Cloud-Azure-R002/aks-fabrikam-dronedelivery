a1490de98f819427e547355807772bcaca7eba8a
amtayaji002 <tyagiamit1975@yahoo.com>
2022-06-23 18:50:54 +0530

    Working sh for auto generation VuePress scaffolding.

--- at-002-0001-vuepress-soln-generator.sh
+++ at-002-0001-vuepress-soln-generator.sh
@@ -20,7 +20,9 @@ warning-text
 ## Project root
 mkdir -p docs/{blog,.vuepress}
 touch docs/README.md
+../at-002-0002-vuepress-edit-readme.sh "docs/README.md"
 
+: << commented-temp
 ## blog directory
 mkdir docs/blog/{css,html,javascript}
 touch docs/blog/{css,html}/{lists.md,README.md}
@@ -35,6 +37,8 @@ mkdir docs/.vuepress/theme/{layouts,styles}
 touch docs/.vuepress/theme/Layout.vue
 touch docs/.vuepress/theme/layouts/{Blog.vue,Home.vue,Post.vue}
 touch docs/.vuepress/theme/styles/{code.styl,custom-blocks.styl}
+
+commented-temp
 
 # Update package.json
 npm install --save-dev json
---
+++ at-002-0002-vuepress-edit-readme.sh
@@ -0,0 +1,8 @@
+
+echo "Editing README ... $1"
+echo '# Baseline Cluster' >> $1
+echo >> $1
+echo '## Details' >> $1
+echo 'This is the baseline cluster for practice.' >> $1
+echo "Path=$(pwd)" >> $1
+echo >> $1
